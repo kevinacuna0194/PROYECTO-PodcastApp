8cad06995edf0431eef4f7711f98ca72dca82d26
Kevin Acuña <94419050+kevinacuna0194@users.noreply.github.com>
2022-12-22 12:16:30 -0300

    Imagemin para aligerar imagenes en Gulp

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ PODCAST FM - Sitio web de plataforma de Podcast en Bootstrap 5 y SASS.
 ~~~
 ---
 <p align="center" font-weight="bold">
-      <img src="https://img.shields.io/badge/ESTADO-EN%20DESARROLLO-informational?style=for-the-badge&logo=Bootstrap&logoWidth=40">
+      <img src="https://img.shields.io/badge/ESTADO-EN%20DESARROLLO-informational?style=social&logo=Bootstrap">
 </p>
 
 :zap::octocat::zap:
--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,11 @@
 const { src, dest, watch } = require('gulp'); /** node_modules/gulp/index.js */ /** src = source - buscar un archivo. dest = Almacenar un archivo. */
+
+/** Compilar CSS */
 const sass = require('gulp-sass')(require('sass'));
+
+/** Minificar Imágenes */
+const imagemin = require('gulp-imagemin');
+
 
 function css(done) {
     /** 1) Identificar el archivo principal 
@@ -15,8 +21,17 @@ function css(done) {
 function dev() {
     /** Puedo poner watch() (nombre de la función que estoy importando) y toma dos parámetros. 1) Archivo que vamos a estar revisando por cambios. 2) Si hay cambios en este archivo, tenemos ya una función que compila SASS. Por lo tanto, manda llamar la función de CSS.*/
     watch('src/scss/**/*.scss', css);
+}
 
+function imagenes(done) {
+    /** 1) Identificar archivo con imágenes */
+    src('src/img/**/*')
+        .pipe(imagemin({ optimizationLevel: 5 })) /** 2) Compilar */
+        .pipe(dest('build/img')) /** 3) Exportarlo o guardarlo en una ubicación */
+
+    done();
 }
 
 exports.css = css;
 exports.dev = dev;
+exports.imagenes = imagenes;

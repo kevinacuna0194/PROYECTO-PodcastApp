33c733b4a7dff3141293445149d79a511cb30e3d
Kevin Acuña <94419050+kevinacuna0194@users.noreply.github.com>
2022-12-22 14:26:18 -0300

    Cambiando Colores y Fuentes

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-const { src, dest, watch, series, parallel } = require('gulp'); /** node_modules/gulp/index.js */ /** src = source - buscar un archivo. dest = Almacenar un archivo. */
+const { src, dest, watch, series } = require('gulp'); /** node_modules/gulp/index.js */ /** src = source - buscar un archivo. dest = Almacenar un archivo. */
 
 /** Compilar CSS */
 const sass = require('gulp-sass')(require('sass'));
@@ -7,12 +7,13 @@ const sass = require('gulp-sass')(require('sass'));
 const imagemin = require('gulp-imagemin');
 
 const autoprefixer = require('autoprefixer');
-const postcss    = require('gulp-postcss')
+const postcss = require('gulp-postcss')
 const sourcemaps = require('gulp-sourcemaps')
-const cssnano = require('cssnano');
+const cssnano = require('gulp-cssnano');
 const notify = require('gulp-notify');
 const cache = require('gulp-cache');
 const webp = require('gulp-webp');
+const plumber = require('gulp-plumber');
 
 
 function css(done) {
@@ -20,10 +21,10 @@ function css(done) {
      * Entonces digamos que va a ir en serie, realiza una tarea y después se va al siguiente pipe() y una vez que finaliza podemos tener otro pipe().
     */
     src('src/scss/app.scss')
+        .pipe(plumber())
         .pipe(sourcemaps.init())
-        .pipe(sass()) /** 2) Compilar SASS */
-        .pipe(postcss([autoprefixer(), cssnano()]))
-        // .pipe(postcss([autoprefixer()]))
+        .pipe(sass())/** 2) Compilar SASS */
+        // .pipe(postcss([autoprefixer(), cssnano()]))
         .pipe(sourcemaps.write('.'))
         .pipe(dest('./build/css')) /** 3) Exportarlo o guardarlo en una ubicación */
 
@@ -33,28 +34,29 @@ function css(done) {
 function imagenes(done) {
     /** 1) Identificar archivo con imágenes */
     src('src/img/**/*')
-        .pipe(cache(imagemin({ optimizationLevel: 3}))) /** 2) Compilar */
-        /** .pipe(imagemin({ optimizationLevel: 5 })) */
+        .pipe(cache(imagemin({ optimizationLevel: 3}))) /** .pipe(imagemin({ optimizationLevel: 5 })) */
         .pipe(dest('build/img')) /** 3) Exportarlo o guardarlo en una ubicación */
-        .pipe(notify({ message: 'Imagen Completada'})); 
+        // .pipe(notify({ message: 'Imagen Completada'}));  
+
     done();
 }
 
-function versionWebp() {
-    return src('src/img/**/*')
+function versionWebp(done) {
+    src('src/img/**/*')
+        .pipe(plumber())
         .pipe( webp() )
         .pipe(dest('build/img'))
-        .pipe(notify({ message: 'Imagen Completada'}));
+        // .pipe(notify({ message: 'Imagen Completada'}));
+
+    done();
 }
 
 function dev() {
     /** Puedo poner watch() (nombre de la función que estoy importando) y toma dos parámetros. 1) Archivo que vamos a estar revisando por cambios. 2) Si hay cambios en este archivo, tenemos ya una función que compila SASS. Por lo tanto, manda llamar la función de CSS.*/
     watch('src/scss/**/*.scss', css);
-    watch('src/img/**/*', imagenes);
-    watch('src/img/**/*', versionWebp);
 }
 
 exports.css = css;
 exports.dev = dev;
 exports.imagenes = imagenes;
-exports.default = parallel( imagenes, versionWebp, css, dev );
+exports.default = series( imagenes, versionWebp, css, dev );
